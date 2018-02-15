cd40760ab22e3aab333da291d9d049da03e1880e
Denis <volchekdenisminsk@gmail.com>
2018-02-15 16:42:45 +0300

    Finis user signup

--- denis.rb
+++ denis.rb
@@ -1,13 +1,13 @@
 class User
-  attr_accessor :name, :email
+  attr_accessor :denis, :penis
 
   def initialize(attributes = {})
     @denis  = attributes[:name]
-    @penis = attributes[:email]
+    @penis = attributes[:age]
   end
 
   def formatted_email
-    "#{@name} <#{@email}>"
+    "#{@denis} <#{@penis}>"
   end
 end
 
